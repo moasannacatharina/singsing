6f4ca236edb6ec0a3994ac121dea6b0bdeba4f84
moasannacatharina <70512179+moasannacatharina@users.noreply.github.com>
2021-03-06 11:36:16 +0100

    added srcset

--- assets/JS/index.js
+++ assets/JS/index.js
@@ -4,4 +4,12 @@ window.addEventListener('load', () => {
   container.classList.add('show-time');
 });
 
-console.log('it works?');
+const urlParams = new URLSearchParams(window.location.search);
+const name = urlParams.get('name');
+const VIP = urlParams.get('VIP');
+
+console.log(name, VIP);
+
+if (VIP == 'true') {
+  window.alert(`hello, ${name}! you are vip!`);
+}
