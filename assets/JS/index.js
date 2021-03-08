0db2c545de11359b92417e0d3e50fd71d0d7543e
moasannacatharina <70512179+moasannacatharina@users.noreply.github.com>
2021-03-08 13:21:06 +0100

    Added sign up-form

--- assets/JS/index.js
+++ assets/JS/index.js
@@ -1,7 +1,35 @@
 const container = document.querySelector('.container');
+const starContainer = document.querySelector('.star-container');
 
 window.addEventListener('load', () => {
   container.classList.add('show-time');
+  starContainer.classList.add('show-time');
+});
+
+const formContainer = document.querySelector('.form-container');
+const rsvpBtn = document.querySelector('.rsvp-btn');
+
+rsvpBtn.addEventListener('click', () => {
+  formContainer.classList.toggle('form-container-visible');
+  window.scroll({
+    top: 1200,
+    left: 0,
+    behavior: 'smooth',
+  });
+});
+
+const confirmationContainer = document.querySelector('.confirmation');
+const confirmationBtn = document.querySelector('.confirmation-btn');
+
+confirmationBtn.addEventListener('click', () => {
+  confirmationContainer.style.display = 'flex';
+});
+
+const programBtn = document.querySelector('.program-btn');
+const program = document.querySelector('.program');
+
+programBtn.addEventListener('click', () => {
+  program.scrollIntoView({ behavior: 'smooth' });
 });
 
 const urlParams = new URLSearchParams(window.location.search);
