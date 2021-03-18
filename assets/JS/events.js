eb70faacac63773764353c50fbc490ad04e73502
moasannacatharina <70512179+moasannacatharina@users.noreply.github.com>
2021-03-18 11:28:29 +0100

    Added notes

--- assets/JS/events.js
+++ assets/JS/events.js
@@ -15,7 +15,23 @@ confirmationBtn.addEventListener('click', () => {
 
 const programBtn = document.querySelector('.program-btn');
 const program = document.querySelector('.program');
+const note2 = document.querySelector('.note-single-2');
+const note3 = document.querySelector('.note-single-3');
+const doubleNote = document.querySelector('.note-double-3');
 
 programBtn.addEventListener('click', () => {
   program.scrollIntoView();
 });
+
+if (window.innerWidth > 1025) {
+  window.addEventListener('scroll', () => {
+    if (window.scrollY > 1500) {
+      note2.style.display = 'block';
+      note2.style.animation = 'note-single-2 1s ease-in-out';
+      note3.style.display = 'block';
+      note3.style.animation = 'note-single-3 1s ease-in';
+      doubleNote.style.display = 'block';
+      doubleNote.style.animation = 'note-double-3 2s ease';
+    }
+  });
+}
--- assets/JS/index.js
+++ assets/JS/index.js
@@ -23,33 +23,3 @@ const addOpacity = () => {
 
 setTimeout(addAnimation, 1700);
 setTimeout(addOpacity, 2700);
-
-//Set session storage to keep in animation from firing again on page reload.
-
-const setSessionStorage = () => {
-  let hasVisited = sessionStorage.getItem('hasVisited');
-  hasVisited = true;
-  sessionStorage.setItem('hasVisited', hasVisited);
-};
-
-setTimeout(setSessionStorage, 5000);
-
-const container = document.querySelector('.container');
-const starContainerMobile = document.querySelector('.star-container');
-const starContainers = document.querySelectorAll('.stars-desktop');
-
-if (sessionStorage.getItem('hasVisited') === 'true') {
-  starContainers.forEach((starContainer) => {
-    starContainer.style.animationName = 'dissolve';
-  });
-  container.style.animationName = 'dissolve';
-  starContainerMobile.style.animationName = 'dissolve';
-  leftBottle.style.animation = 'left-bottle 1s infinite';
-  leftBottle.style.opacity = '1';
-
-  rightBottle.style.opacity = '1';
-  rightBottle.style.animation = 'right-bottle 1s infinite';
-
-  redMic.style.animationName = 'dissolve';
-  redMic.style.opacity = '1';
-}
